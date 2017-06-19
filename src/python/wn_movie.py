e30467f708c69402f93d518483ea2b6c8b97dbf1
Bradley Meyer <mbradle@clemson.edu>
2017-06-19 17:12:56 -0400

    Add title.

--- src/python/wn_movie.py
+++ src/python/wn_movie.py
@@ -23,6 +23,8 @@ def movie_mass_fractions_vs_property(
 
   writer = FFMpegWriter( fps=fps, metadata=metadata )
 
+  ts_factor = {'yr': 3.15e7, 'kyr': 3.15e10, 'Myr': 3.15e13}
+
   fig = plt.figure()
 
   groups = wn_h5.get_iterable_groups( file )
@@ -42,6 +44,16 @@ def movie_mass_fractions_vs_property(
   with writer.saving(fig, movie_name, 200 ):
      for group in groups:
         m = wn_h5.get_group_mass_fractions( file, group )
+
+        t = wn_h5.get_group_properties_in_zones( file, group, ['time'] )
+        tt = float( t['time'][0] )
+        title_str = "time (s) = %7.2e" % tt
+        if( 'time_scale' in keyword_parameters ):
+            title_str = \
+               "time (" + keyword_parameters['time_scale'] + ") = %7.2e" \
+               % (tt / ts_factor[keyword_parameters['time_scale']] )
+ 
+        plt.title( title_str )
 
         if prop == '':
            x = np.linspace( 0, len( m[:,0] ) - 1, len( m[:,0] ) )
